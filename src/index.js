6fa47b53ccdf0f686b927fd2b4545a8900222fd8
Ekaterina Denisenko <lira-gde-to@rambler.ru>
2019-03-24 15:17:22 +0300

    feat: add answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let string = str;
+  for (i = string.length; i >= 0; i--){
+    for (let bracket = 0; bracket < bracketsConfig.length; bracket++){
+      const bracketsPair = bracketsConfig[bracket].join('');
+      if (string.includes(bracketsPair)) {
+        string = string.replace(bracketsPair, '');
+      }
+    }
+  }
+  return string === "";
 }
+
